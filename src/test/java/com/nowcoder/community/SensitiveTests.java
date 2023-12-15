733b1275dbbb9d486e59e279fff5a0e075812ddd
shawenher <2842368912@qq.com>
2023-12-16 05:29:28 +0800

    这是新添加的文件

---
+++ src/test/java/com/nowcoder/community/SensitiveTests.java
@@ -0,0 +1,25 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.SensitiveFilter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class SensitiveTests {
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSensitiveFilter(){
+        String test = "这里不可以赌博，@@@嫖@@娼##，吸毒，也不可以开票票。。";
+        test = sensitiveFilter.filter(test);
+        System.out.println(test);
+    }
+}
